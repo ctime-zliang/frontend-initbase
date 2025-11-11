29983f714112c3dc8f4a4415d7ca039b1b597a5c
zhangliang <zhangliang@creality.com>
2025-11-11 19:54:36 +0800

    feat: modify - daily commit

--- srcReact/app/App.tsx
+++ srcReact/app/App.tsx
@@ -5,23 +5,26 @@ import '../../node_modules/antd/dist/antd.css'
 import favicon from './assets/images/log.jpg'
 import { TCommonComponentBaseProps } from './types/comm.types'
 import { RootMemo } from './layout/Root'
+import styled from 'styled-components'
 
-const appContainerStyle: React.CSSProperties = {
-	display: 'flex',
-	flexDirection: 'column',
-	flexWrap: 'nowrap',
-	justifyContent: 'space-between',
-}
+const Container = styled.section`
+	display: flex;
+	flex-drection: column;
+	justify-content: space-between;
+	align-content: center;
+	align-items: center;
+	flex-wrap: nowrap;
+`
 
 export default function App(props: TCommonComponentBaseProps): React.ReactElement {
 	console.log(`App ☆☆☆`, props)
 	const __app_id__: number = Math.random()
 	return (
-		<section data-tagitem="React-App" className="react-app" style={appContainerStyle}>
+		<Container data-tagitem="React-App" className="react-app">
 			<Helmet link={[{ rel: 'icon', type: 'image/jpg', href: favicon }]}>
 				<title>React Application</title>
 			</Helmet>
 			<RootMemo __AppProps__={{ __app_id__ }} {...props} />
-		</section>
+		</Container>
 	)
 }
